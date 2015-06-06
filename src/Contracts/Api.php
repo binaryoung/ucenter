626ea6f141067ce8b7cd8117941de73991a012a3
young <i@xiayang.me>
2015-06-06 20:40:14 +0800

    优化代码

--- src/Contracts/Api.php
+++ src/Contracts/Api.php
@@ -2,34 +2,37 @@
 
 interface Api
 {
+    public $get = [];
 
-    public static function test($get, $post);
+    public $post = [];
+   
+    public static function test();
 
-    public static function deleteuser($get, $post);
+    public static function deleteuser();
 
-    public static function renameuser($get, $post);
+    public static function renameuser();
 
-    public static function updatepw($get, $post);
+    public static function updatepw();
 
-    public static function gettag($get, $post);
+    public static function gettag();
 
-    public static function synlogin($get, $post);
+    public static function synlogin();
 
-    public static function synlogout($get, $post);
+    public static function synlogout();
 
-    public static function updatebadwords($get, $post);
+    public static function updatebadwords();
 
-    public static function updatehosts($get, $post);
+    public static function updatehosts();
 
-    public static function updateapps($get, $post);
+    public static function updateapps();
 
-    public static function updateclient($get, $post);
+    public static function updateclient();
 
-    public static function updatecredit($get, $post);
+    public static function updatecredit();
 
-    public static function getcreditsettings($get, $post);
+    public static function getcreditsettings();
 
-    public static function updatecreditsettings($get, $post);
+    public static function updatecreditsettings();
 
-    public static function getcredit($get, $post);
+    public static function getcredit();
 }
--- src/Controllers/ApiController.php
+++ src/Controllers/ApiController.php
@@ -8,26 +8,11 @@ use Binaryoung\Ucenter\Services\Help;
 class ApiController extends Controller 
 {
     use Help;
-    
+
 	public function __construct()
 	{
 		define('UC_CLIENT_VERSION', '1.6.0');    //note UCenter 版本标识
 		define('UC_CLIENT_RELEASE', '20110501');
-
-		define('API_DELETEUSER', 1);        //note 用户删除 API 接口开关
-		define('API_RENAMEUSER', 1);        //note 用户改名 API 接口开关
-		define('API_GETTAG', 1);        //note 获取标签 API 接口开关
-		define('API_SYNLOGIN', 1);        //note 同步登录 API 接口开关
-		define('API_SYNLOGOUT', 1);        //note 同步登出 API 接口开关
-		define('API_UPDATEPW', 1);        //note 更改用户密码 开关
-		define('API_UPDATEBADWORDS', 1);    //note 更新关键字列表 开关
-		define('API_UPDATEHOSTS', 1);        //note 更新域名解析缓存 开关
-		define('API_UPDATEAPPS', 1);        //note 更新应用列表 开关
-		define('API_UPDATECLIENT', 1);        //note 更新客户端缓存 开关
-		define('API_UPDATECREDIT', 1);        //note 更新用户积分 开关
-		define('API_GETCREDITSETTINGS', 1);    //note 向 UCenter 提供积分设置 开关
-		define('API_GETCREDIT', 1);        //note 获取用户的某项积分 开关
-		define('API_UPDATECREDITSETTINGS', 1);    //note 更新应用积分设置 开关
 
 		define('API_RETURN_SUCCEED', 1);
 		define('API_RETURN_FAILED', -1);
@@ -70,8 +55,13 @@ class ApiController extends Controller
         ];
         
         if (in_array($action, $actionList) && method_exists($api, $action)) {
-        	$post = self::unserialize(file_get_contents('php://input'));
-            return $api::$action($get, $post);
+        	
+            $post = self::unserialize(file_get_contents('php://input'));
+
+            $api->get = $get;
+            $api->post = $post;
+
+            return $api::$action();
         } else {
             return API_RETURN_FAILED;
         }
